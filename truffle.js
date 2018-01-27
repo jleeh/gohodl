4ff45dda3ab4b9c277e15187cdf17e08dd74a9d3
Jonny Huxtable <jonnyh@outlook.com>
2018-01-27 15:27:05 +0000

    Added mainnet deployment

--- truffle.js
+++ truffle.js
@@ -9,19 +9,20 @@ module.exports = {
       network_id: 3,
       host: "geth.ropsten.lp",
       port:  8545,
-      gas:   4712388,
+      gas:   2680000,
       from: "0xab12e6280e1024825f4880268a9c9618a2dca3f5"
     },
     mainnet:  {
       network_id: 1,
       host: "geth.lp",
       port:  8545,
-      gas:   4712388,
+      gas:   5000,
       from: "0x32D1A6516d0a2174f4cd0D7a30Aa7962d532c98C"
     }
   },
   rpc: {
     host: 'localhost',
     post: 8080
-  }
+  },
+  solc: { optimizer: { enabled: true, runs: 200 } }
 };
